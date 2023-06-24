ba668b2892ce8a455ca2ec9db839c53b08d8574a
reetesh105 <reeteshyadav9415@gmail.com>
2023-06-24 19:30:39 +0530

    My user management system

---
+++ src/data/index.js
@@ -0,0 +1,27 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'ritesh',
+    lastName: 'yadav',
+    email: 'reetesh@example.com',
+    phone: '1234567890'
+  },
+  {
+    id: 2,
+    firstName: 'Adrienne',
+    lastName: 'Doak',
+    email: 'adrienne@example.com',
+    phone: '098765432'
+  
+  },
+  {
+    id: 3,
+    firstName: 'Rolf',
+    lastName: 'Hegdal',
+    email: 'rolf@example.com',
+    phone: '4567897587'
+
+  },
+];
+
+export { employeesData };
